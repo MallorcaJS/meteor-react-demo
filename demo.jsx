55f919f474d365e28722b32b08a46e2aa531e8ad
Rafa Dorado <rafa@ventafun.com>
2015-12-12 17:38:27 +0100

    Multiple nodes in one React Component Error

--- demo.jsx
+++ demo.jsx
@@ -1,8 +1,14 @@
 var Demo = React.createClass({
 	render(){
-		return (<h1>Hello World from React Component</h1>)
+
+		return (
+			<h1>Hello World from React Component</h1><p>NEW NODE</p>
+			// console.server log => demo.jsx:5:44: demo.jsx: Adjacent JSX elements must be wrapped in an enclosing tag (5:44)
+	)
 	}
+
 });
+
 
 
 //Meteor internal procedure that executes when the App Starts
