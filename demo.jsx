13b96e412a14f028c1aa6467abc2071fef38a6de
Rafa Dorado <rafa@ventafun.com>
2015-12-12 17:35:04 +0100

    First React Component Created and Called

--- demo.jsx
+++ demo.jsx
@@ -1,7 +1,14 @@
+var Demo = React.createClass({
+	render(){
+		return (<h1>Hello World from React Component</h1>)
+	}
+});
+
+
 //Meteor internal procedure that executes when the App Starts
 Meteor.startup( function(){
 	if(Meteor.isClient) {
-		ReactDOM.render(<h1>Hello World</h1>, document.querySelector("#demo-react-app"));
+		ReactDOM.render(<Demo/>, document.querySelector("#demo-react-app"));
 		// Check Browser Console to read React Warning
 	}
 });
