367797f6b89d908566bedeaf38aad97b3b2c11ea
Rafa Dorado <rafa@ventafun.com>
2015-12-12 17:58:28 +0100

    Printing initial state of Demo Component

--- demo.jsx
+++ demo.jsx
@@ -1,14 +1,28 @@
 //Talk about autopublish / pub-sub paradigm in Meteor
-People = new Mongo.Collection("People");
+//People = new Mongo.Collection("People");
 
 
 var Demo = React.createClass({
+	getInitialState(){
+		return {
+			people: [{name:'Kevin',_id:'1'},{name:'Rafa',_id:'2'}]
+		}
+	},
 	render(){
 
 		return (
 			<div>
 				<input type="text"/>
 				<button>Hola</button>
+				<ul>
+					{
+						this.state.people.map(
+							(person) => {
+								return <li key={person._id}>{person.name}</li>
+							}
+						)
+					}
+				</ul>
 			</div>
 		)
 	}
