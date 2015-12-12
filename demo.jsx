d980d8d11e329a319f6cff310514c93e5f9d3931
Rafa Dorado <rafa@ventafun.com>
2015-12-12 17:32:09 +0100

    React warning fixed. Application Wrapper added

--- demo.jsx
+++ demo.jsx
@@ -1,7 +1,7 @@
 //Meteor internal procedure that executes when the App Starts
 Meteor.startup( function(){
 	if(Meteor.isClient) {
-		ReactDOM.render(<h1>Hello World</h1>, document.body);
+		ReactDOM.render(<h1>Hello World</h1>, document.querySelector("#demo-react-app"));
 		// Check Browser Console to read React Warning
 	}
 });
