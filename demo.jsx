4fc548cfa42ff2d5a3e952c55d2daf1b3ef053af
Kevin O'Hagan <kevinohagan@gmail.com>
2015-12-12 17:20:55 +0100

    passed logic of selectedPerson to parent component

--- demo.jsx
+++ demo.jsx
@@ -5,38 +5,34 @@ let Person = React.createClass({
   propTypes:{
 
     name: React.PropTypes.string,
+    id: React.PropTypes.string,
     changePerson: React.PropTypes.func,
+    selectPerson: React.PropTypes.func,
+    editing: React.PropTypes.bool
 
-  },
-
-  getInitialState(){
-    return {
-      editing: false,
-    }
-  },
-
-  edit(){
-     this.setState({
-      editing: !this.state.editing
-     })
   },
 
   changeName(event){
     if (event.charCode === 13){
-      People.update({_id: this.props.key},{$set:{ name: this.refs.input.value}});
+      People.update({_id: this.props.id},{$set:{ name: this.refs.input.value}});
+      this.select();
     }
+  },
 
-
+  clickPerson(event){
+    event.preventDefault();
+    this.props.selectPerson(this.props.id);
+    this.refs.input.value = "";
   },
 
   render(){
-    if (this.state.editing){
+    if (this.props.editing){
       return (
           <input ref="input" type="text" defaultValue={this.props.name} onKeyPress={this.changeName}/>
         )
     } else {
       return (
-        <li onClick={this.edit}> {this.props.name}</li>
+        <li onClick={this.clickPerson}> {this.props.name}</li>
       )
     }
   }
@@ -55,12 +51,37 @@ let Demo = React.createClass({
     }
   },
 
+  getInitialState(){
+    return {
+      selectedPerson: null
+    }
+  },
+
+
 
   // insertName method
   insertName(){
     console.log(this.refs.name.value);
     People.insert({name: this.refs.name.value });
   },
+
+  selectPerson(id){
+    console.log(id);
+
+    this.setState({
+      selectedPerson: id
+    });
+
+  },
+
+  isEditing(id){
+    if ( this.state.editing === id){
+      return true;
+    } else {
+      return false
+    }
+  },
+
 
 
   render() {
@@ -71,10 +92,9 @@ let Demo = React.createClass({
         <button onClick={this.insertName}>Ok</button>
         <ul>
           {
-            this.data.people.map(function(person){
+            this.data.people.map((person) => {
               return (
-                <Person key={person._id} name={person.name}/>
-                // <li key={name._id}><input type="text" value={name.name} /></li>                  
+                <Person selectPerson={this.selectPerson} key={person._id} id={person._id} name={person.name} editing={person._id === this.state.selectedPerson} />
               )
             })
           }
