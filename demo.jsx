26509c128eaeca2565729cacc5db19e57afbcb93
Rafa Dorado <rafa@ventafun.com>
2015-12-12 18:07:19 +0100

    Insert Person to Mongo Collection

--- demo.jsx
+++ demo.jsx
@@ -1,22 +1,39 @@
 //Talk about autopublish / pub-sub paradigm in Meteor
-//People = new Mongo.Collection("People");
+People = new Mongo.Collection("People");
 
 
 var Demo = React.createClass({
+	mixins: [ReactMeteorData],
+
 	getInitialState(){
 		return {
-			people: [{name:'Kevin',_id:'1'},{name:'Rafa',_id:'2'}]
+			//people: [{name:'Kevin',_id:'1'},{name:'Rafa',_id:'2'}]
 		}
+	},
+	getMeteorData(){
+		return {
+			// Return all People in collection
+			people: People.find({}).fetch()
+		}
+	},
+	insertPerson(){
+		let personName = this.refs.inputPersonName;
+		// Simple Length Checking
+		if(personName.length < 3) return;
+		// Insert to Mongo
+		People.insert({name:personName.value});
+		//Clean INput text
+		personName.value = "";
 	},
 	render(){
 
 		return (
 			<div>
-				<input type="text"/>
-				<button>Hola</button>
+				<input type="text" ref="inputPersonName" placeholder="Nombre Persona"/>
+				<button onClick={this.insertPerson}>Guardar</button>
 				<ul>
 					{
-						this.state.people.map(
+						this.data.people.map(
 							(person) => {
 								return <li key={person._id}>{person.name}</li>
 							}
