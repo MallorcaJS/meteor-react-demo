f23f95c6361a95e8424ad2dafef73481499f13ce
Kevin O'Hagan <kevinohagan@gmail.com>
2015-12-12 13:04:42 +0100

    added names collection and display the list of names

--- demo.jsx
+++ demo.jsx
@@ -1,16 +1,36 @@
 
+Names = new Mongo.Collection('names');
+
 
 let Demo = React.createClass({
+
+  mixins: [ReactMeteorData],
+
+  getMeteorData(){
+    return {
+      names: Names.find().fetch()
+    }
+  },
 
   hello(){
     console.log(this.refs.name.value);
   },
 
   render() {
+    console.log(this.data.names);
     return (
       <div>
         <input ref="name"/>
         <button onClick={this.hello}>Ok</button>
+        <ul>
+          {
+            this.data.names.map(function(name){
+              return (
+                <li key={name._id}>{name.name}</li>
+              )
+            })
+          }
+        </ul>
       </div>
     )
   }
